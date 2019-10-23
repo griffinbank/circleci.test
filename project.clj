9bca6487f0e1bbcfe0f2ae8724e3b774cf2496fb
Allen Rohner <allen@griffin.sh>
2019-10-23 13:49:18 -0500

    Update project

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject io.synple/circleci.test "0.5.0-alpha2"
+(defproject griffinbank/circleci.test "0.5.0-alpha2"
   :description "clojure.test compatible test-runner"
   :url "https://github.com/circleci/circleci.test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :pedantic? :abort
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.xml "0.0.8"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.xml "0.2.0-alpha5"]]
   :profiles {:alpha-data-xml
              {:dependencies [[org.clojure/data.xml "0.2.0-alpha5"]]}}
   :aliases {"test" ["run" "-m" "circleci.test/dir" :project/test-paths]
